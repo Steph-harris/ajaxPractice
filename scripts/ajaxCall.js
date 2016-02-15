2893bd7b3c41883a06b656a8ee7d0efae03a0e64
steph-harris <steph.harris21@gmail.com>
2016-02-15 12:40:17 -0500

    commit for gh pages

--- scripts/ajaxCall.js
+++ scripts/ajaxCall.js
@@ -5,17 +5,17 @@ $(document).ready(function(){
     $.ajax({
       url: "https://en.wikipedia.org/w/api.php",
       data: {
-          format: "json",
-          action: "parse",
-          page: title,
-          prop:"text",
-          section:0,
+        format: "json",
+        action: "parse",
+        page: title,
+        prop:"text",
+        section:0,
       },
       dataType: 'jsonp',
       headers: {
           'Api-User-Agent': 'MyCoolTool/1.1 (http://example.com/MyCoolTool/; MyCoolTool@example.com) BasedOnSuperLib/1.4'
       },
-      success: function (data) {
+      success: function (data) {debugger;
           console.log(data)
       //$("#article").html(data.parse.text["*"])
           
@@ -37,6 +37,24 @@ $(document).ready(function(){
     });
   });
 });
+//alternate calls:
+// // var url="http://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + breedInfo +"&redirects&prop=text&callback=?";
+    
+//     // //https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json
+//     // debugger;
+//     //$(".infoBox").empty();
+//     $("#wikiInfo").empty();
+
+//     // $.getJSON(url,function(data){
+//     //   wikiHTML = data.parse.text["*"];
+//     //   $wikiDOM = $("<p>"+wikiHTML+"</p>");
+//     //   $(".infoBox").append($wikiDOM);
+
+//     $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=' + breedInfo +'&prop=text&format=json&callback=?', function(json) { 
+//       $("#wikiInfo").html(json.parse.text["*"]); 
+//       $("#wikiInfo").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
+//       $("#wikiInfo").find("a").attr("target", "_blank"); 
+//     });
 
 
 ///w/api.php?action=query&prop=info&format=json&titles=schnauzer
